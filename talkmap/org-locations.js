f05aadeeeffdc4693edd2d11c4fa47c4df7c65ef
Willow B. Kion-Crosby <wbk7@scarletmail.rutgers.edu>
2018-09-07 14:56:12 -0400

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,18 +1,8 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
-  ],
-  [
-    "London, UK",
-    51.5073219,
-    -0.1276473
-  ],
-  [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Piscataway, NJ",
+    40.5231,
+    74.4649
   ],
   [
     "Los Angeles, CA",
